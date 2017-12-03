a94ccc5728511214c5c1190487535bc7c125ba76
Patrick <piasecki.patrick.15@gmail.com>
2017-12-03 10:58:11 +0100

    empty project

---
+++ src/main/java/pl/piasecki/jakeapp/JakeappApplication.java
@@ -0,0 +1,12 @@
+package pl.piasecki.jakeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakeappApplication.class, args);
+	}
+}
